8c44b34473bdc7fcc1c124099f4b83ce51d53956
aidabyte <ablinstrubyte@gmail.com>
2022-07-21 23:21:17 -0500

    jquery in script

---
+++ js/index.js
@@ -0,0 +1,19 @@
+$(".typewriter").typeIt({
+     whatToType: ["Hi, I'm Aida Blinstrubyte"],
+     typeSpeed: 100
+});
+
+
+// $(document).ready(function(){
+    
+//     $("#workText").mouseover(function(){
+//       $(this).animate({fontSize: '60px'});
+//       $("#workText").mouseout(function(){
+//         $(this).animate({fontSize: '50px'});
+       
+//     });
+//   });
+// });
+// ;
+
+
