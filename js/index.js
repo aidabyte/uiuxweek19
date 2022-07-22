a6d24defbfdb621634c39e6425388282d0a09395
aidabyte <ablinstrubyte@gmail.com>
2022-07-21 23:25:18 -0500

    added javascript mouseover

--- js/index.js
+++ js/index.js
@@ -1,19 +1,13 @@
-$(".typewriter").typeIt({
-     whatToType: ["Hi, I'm Aida Blinstrubyte"],
-     typeSpeed: 100
-});
-
-
-// $(document).ready(function(){
+$(document).ready(function(){
     
-//     $("#workText").mouseover(function(){
-//       $(this).animate({fontSize: '60px'});
-//       $("#workText").mouseout(function(){
-//         $(this).animate({fontSize: '50px'});
+    $("#workText").mouseover(function(){
+      $(this).animate({fontSize: '60px'});
+      $("#workText").mouseout(function(){
+        $(this).animate({fontSize: '50px'});
        
-//     });
-//   });
-// });
-// ;
+    });
+  });
+});
+;
 
 
