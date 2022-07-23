6086ee1657ca9ce5e2530ecc645ba470044f9753
aidabyte <ablinstrubyte@gmail.com>
2022-07-22 22:08:21 -0500

    making changes

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     
-    $("#workText").mouseover(function(){
-      $(this).animate({fontSize: '60px'});
-      $("#workText").mouseout(function(){
-        $(this).animate({fontSize: '50px'});
+    $(".clickMyResume").mouseover(function(){
+      $(this).animate({width: "560px", height:"500px",fontSize:"100px"});
+      $(".ClickMyResume").mouseout(function(){
+        $(this).animate({width: "260px", height:"50px"});
        
     });
   });
